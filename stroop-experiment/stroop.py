90a5666f012a17c977652e5486c034767770b8af
iamlumiya <l5kang@ucsd.edu>
2025-02-17 00:07:34 -0800

    # 6 stroop.py

--- stroop-experiment/stroop.py
+++ stroop-experiment/stroop.py
@@ -16,7 +16,8 @@ placeholder = visual.Rect(win,width=180,height=80, fillColor="lightgray",lineCol
 word_stim = visual.TextStim(win,text="", height=40, color="black",pos=[0,0])
 instruction = visual.TextStim(win,text="Press the first letter of the ink color", height=20, color="black",pos=[0,-200], autoDraw = True)
 fixation = visual.TextStim(win, text = "+", color = "black", height = 15, pos = [0, 0])
-feedback = visual.TextStim(win, text = "Incorrect", color = "black", height = 40, pos = [0, 0])q
+feedback = visual.TextStim(win, text = "Incorrect", color = "black", height = 40, pos = [0, 0])
+feedback2 = visual.TextStim(win, text = "Too Slow", color = "black", height = 40, pos = [0,0])
 
 # Experiment loop
 RTs = []
@@ -48,24 +49,30 @@ while True:
     
     # Wait for response and record RT    
     valid_keys = ["r", "o", "y", "g", "b", "q"]
-    key_pressed = event.waitKeys(keyList = valid_keys)
-    
-    if key_pressed[0] == "q":
-        break
-    else:
-        RT = round(responseTimer.getTime()) * 1000
-        RTs.append(RT)
-        print("Response:", key_pressed[0], "Reaction Time:", RT)
-        
-    # Display feedback message only for incorrect response
-    if key_pressed[0] == cur_stim[0]:
-        pass
-    elif key_pressed[0] == "q":
-        break
-    else:
-        feedback.draw()
+    key_pressed = event.waitKeys(keyList = valid_keys, maxWait = 2)
+
+    if not key_pressed:
+        feedback2.draw()
         win.flip()
         core.wait(1)
+        RT = 0
+    else: 
+        if key_pressed[0] == "q":
+            break
+            
+        RT = round(responseTimer.getTime() * 1000)
+        RTs.append(RT)
+        print("Response:", key_pressed[0], "Reaction Time:", RT)
+    
+        # Display feedback message only for incorrect response
+        if key_pressed[0] == cur_stim[0]:
+            pass
+        elif key_pressed[0] == "q":
+            break
+        else:
+            feedback.draw()
+            win.flip()
+            core.wait(1)
     
     # Blank space for 0.15 second
     placeholder.draw()
